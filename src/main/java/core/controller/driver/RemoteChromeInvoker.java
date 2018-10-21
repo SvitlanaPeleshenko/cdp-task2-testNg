f102b2f46878e85e74a6b900775a25758e950561
SvitlanaPeleshenko <peleshenko.svitlana@gmail.com>
2018-10-21 22:59:52 +0300

    .

--- src/main/java/core/controller/driver/RemoteChromeInvoker.java
+++ src/main/java/core/controller/driver/RemoteChromeInvoker.java
@@ -6,22 +6,27 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 
 
-/**
- * Created by Svitlana_Peleshenko on 7/31/2018.
- */
 public class RemoteChromeInvoker implements WebDriverInvoker {
 
     public WebDriver invokeWebDriver() {
         TestProperties properties = TestProperties.getInstance();
+
         String hubURL = String.format("%s:%s/wd/hub", properties.selenoidHost(),
                 properties.selenoidPort());
+
         System.out.println("Init remote driver, Hub URL: " + hubURL);
-        DesiredCapabilities capability = DesiredCapabilities.chrome();
+
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setVersion("70.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+
         try {
-            return new RemoteWebDriver(new URL(hubURL), capability);
+            return new RemoteWebDriver(new URL(hubURL), capabilities);
         } catch (Exception e) {
             throw new RuntimeException("failed to initialize selenoid chrome driver", e);
         }
